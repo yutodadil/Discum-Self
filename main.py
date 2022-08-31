dc5c4424642c2ca93c663cb4accbc7d9a2dff135
Milked/Milkey_Hack <70030251+yutodadil@users.noreply.github.com>
2022-08-31 21:46:50 +0900

    Add New Feature
    Usage: {mainprefix}mainbio [bio]

--- main.py
+++ main.py
@@ -1483,6 +1483,88 @@ async def leave(ctx, svrid):
   print(r)
   await ctx.send(r)
 
+@bot2.command()
+async def mainbio(ctx, naiyou):
+  await ctx.message.delete()
+  dt = datetime.datetime.today()
+  if len(str(dt.month)) == 1:
+      if len(str(dt.day)) == 1:
+          r = httpx.patch(f'https://discord.com/api/v9/users/@me/profile',  proxies='socks5://127.0.0.1:9050', headers={
+                  "Authorization": __config__['bot']['maintoken'],
+                  "accept": "*/*",
+                  "accept-language": "ja-JP",
+                  "Accept-Encoding": "gzip, deflate, br",
+                  "connection": "keep-alive",
+                  "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=ja-JP',
+                  "DNT": "1",
+                  "origin": "https://discord.com",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "referer": "https://discord.com/channels/@me",
+                  "TE": "Trailers",
+                  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36",
+                  "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+              }, json={"bio": f"{naiyou}\nLast Edit:{dt.year}/0{dt.month}/0{dt.day} {dt.hour}:{dt.minute}:{dt.second}"})
+      else:
+          r = httpx.patch(f'https://discord.com/api/v9/users/@me/profile',  proxies='socks5://127.0.0.1:9050', headers={
+                  "Authorization": __config__['bot']['maintoken'],
+                  "accept": "*/*",
+                  "accept-language": "ja-JP",
+                  "Accept-Encoding": "gzip, deflate, br",
+                  "connection": "keep-alive",
+                  "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=ja-JP',
+                  "DNT": "1",
+                  "origin": "https://discord.com",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "referer": "https://discord.com/channels/@me",
+                  "TE": "Trailers",
+                  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36",
+                  "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+              }, json={"bio": f"{naiyou}\nLast Edit:{dt.year}/0{dt.month}/{dt.day} {dt.hour}:{dt.minute}:{dt.second}"})
+  else:
+      if len(str(dt.day)) == 1:
+          r = httpx.patch(f'https://discord.com/api/v9/users/@me/profile',  proxies='socks5://127.0.0.1:9050', headers={
+                  "Authorization": __config__['bot']['maintoken'],
+                  "accept": "*/*",
+                  "accept-language": "ja-JP",
+                  "Accept-Encoding": "gzip, deflate, br",
+                  "connection": "keep-alive",
+                  "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=ja-JP',
+                  "DNT": "1",
+                  "origin": "https://discord.com",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "referer": "https://discord.com/channels/@me",
+                  "TE": "Trailers",
+                  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36",
+                  "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+              }, json={"bio": f"{naiyou}\nLast Edit:{dt.year}/{dt.month}/0{dt.day} {dt.hour}:{dt.minute}:{dt.second}"})
+      else:
+          r = httpx.patch(f'https://discord.com/api/v9/users/@me/profile',  proxies='socks5://127.0.0.1:9050', headers={
+                  "Authorization": __config__['bot']['maintoken'],
+                  "accept": "*/*",
+                  "accept-language": "ja-JP",
+                  "Accept-Encoding": "gzip, deflate, br",
+                  "connection": "keep-alive",
+                  "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=ja-JP',
+                  "DNT": "1",
+                  "origin": "https://discord.com",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "referer": "https://discord.com/channels/@me",
+                  "TE": "Trailers",
+                  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36",
+                  "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+              }, json={"bio": f"{naiyou}\nLast Edit:{dt.year}/{dt.month}/{dt.day} {dt.hour}:{dt.minute}:{dt.second}"})
+  print(r)
+  await ctx.send(f"Change Bios, Change to {naiyou}\nCode -> {r}")
+# Thanks for Probmkrnew, Its Good Idea.
+	
 @bot2.event
 async def on_ready():
   global start, music
